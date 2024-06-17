6ba4cb3942465c1f13949b994124ac830d1cf41f
Cristian117-343 <castillo.perez.cy@gmail.com>
2024-06-17 06:58:26 -0600

    Add files via upload

--- src/bienesraices.sql
+++ src/bienesraices.sql
@@ -2,24 +2,26 @@ CREATE DATABASE BIENESRAICES;
 
 USE BIENESRAICES;
 
-CREATE TABLE USUARIO(
-NombreUsuario varchar(10) not null,
-ApePatUsuario varchar(10) not null,
-ApeMatUsuario varchar(10) not null,
-Correo varchar(20) not null,
-Telefono int not null,
-Mensaje varchar(100),
-primary key (Correo)
-);
+CREATE TABLE propiedades (
+ id int NOT NULL,
+ titulo varchar(45) DEFAULT NULL,
+ precio decimal(10,2) DEFAULT NULL,
+ imagen varchar(200) DEFAULT NULL,
+ descripcion longtext,
+ habitaciones int DEFAULT NULL,
+ wc int DEFAULT NULL,
+ estacionamiento int DEFAULT NULL,
+ creado date DEFAULT NULL,
+ vendedores_id int NOT NULL,
+ PRIMARY KEY (id),
+ KEY fk_propiedades_vendedores_idx (vendedores_id),
+ CONSTRAINT fk_propiedades_vendedores FOREIGN KEY (vendedores_id) REFERENCES vendedores(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-CREATE TABLE PROPIEDAD(
-Propiedad int auto_increment,
-Tipo varchar(7) not null,
-PrecioPresupuesto int not null,
-Contacto varchar(10) not null,
-Fecha date not null,
-Hora time not null,
-Correo varchar(20),
-primary key(Propiedad),
-foreign key (Correo) references USUARIO(Correo)
-);
+CREATE TABLE vendedores(
+ id int NOT NULL AUTO_INCREMENT,
+ nombre varchar(45) DEFAULT NULL,
+ apellido varchar(45) DEFAULT NULL,
+ telefono varchar(10) DEFAULT NULL,
+ PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3
